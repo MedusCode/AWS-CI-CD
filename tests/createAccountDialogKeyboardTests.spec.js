4bbb7183f3b3a3745aea5432f5125b6ca2bacd9b
MedusCode <87340589+MedusCode@users.noreply.github.com>
2025-06-06 17:14:54 +0400

    Fix tests and add test job to GitHub actions

--- tests/createAccountDialogKeyboardTests.spec.js
+++ tests/createAccountDialogKeyboardTests.spec.js
@@ -19,7 +19,7 @@ test.describe("create account keyboard tests",() => {
       await expect(page.locator('#acctEmail')).toBeFocused(); //Email field has focus
     });
 
-    test('invalid email and repeated password via kb interface', async ({ page }) => { 
+    test('invalid email and repeated password via kb interface', async ({ page }) => {
         await page.keyboard.type('chris.h'); //Invalid email
         await page.keyboard.press('Tab'); //Focus on 'Password' field
         await expect(page.locator('#acctPassword')).toBeFocused();
@@ -28,22 +28,11 @@ test.describe("create account keyboard tests",() => {
         await expect(page.locator('#acctPasswordRepeat')).toBeFocused();
         await page.keyboard.type('Speedgolf1234'); //Password does not match.
         await page.keyboard.press('Tab'); //Focus on 'Display Name' field
-        await expect(page.locator('#acctDisplayName')).toBeFocused();
-        await page.keyboard.press('Tab'); //Focus on 'Profile Pic' field
-        await expect(page.locator('#acctProfilePic')).toBeFocused();
-        await page.keyboard.press('Tab'); //Focus on 'Security Q' field 
-        await expect(page.locator('#acctSecurityQuestion')).toBeFocused();
-        await page.keyboard.press('Tab'); //Focus on 'Security A' field
-        await expect(page.locator('#acctSecurityAnswer')).toBeFocused();
-        await page.keyboard.press('Tab'); //Focus on 'Create Account' Button
         await page.keyboard.press('Enter'); //Activate 'Create Account' Button
         await expect(page.locator('#acctEmailError')).toBeVisible(); //Email error visible
-        await expect(page.locator('#acctEmailError')).toBeFocused(); //top-most error should have focus 
+        await expect(page.locator('#acctEmailError')).toBeFocused(); //top-most error should have focus
         await expect(page.locator('#acctPasswordError')).not.toBeVisible(); //No password error
         await expect(page.locator('#acctPasswordRepeatError')).toBeVisible(); //No password error
-        await expect(page.locator('#acctDisplayNameError')).toBeVisible(); //No password error
-        await expect(page.locator('#acctSecurityQuestionError')).toBeVisible(); //No password error
-        await expect(page.locator('#acctSecurityAnswerError')).toBeVisible(); //No password error
         await page.keyboard.press('Enter'); //Activate "Enter a valid email address"
         await expect(page.locator('#acctEmail')).toBeFocused(); //Email field should be focused
       });
--- tests/createAccountDialogVisualTests.spec.js
+++ tests/createAccountDialogVisualTests.spec.js
@@ -9,23 +9,20 @@ test.describe("create account visual tests",() => {
       await expect(page.locator('#acctEmail')).toBeFocused(); //Email field has focus
 
     });
-  
-    test('invalid email and repeated password via visual interface', async ({ page }) => { 
+
+    test('invalid email and repeated password via visual interface', async ({ page }) => {
       await page.keyboard.type('chris.h'); //Invalid email
       await page.click('#acctPassword'); //Focus on 'Password' field
       await expect(page.locator('#acctPassword')).toBeFocused();
       await page.keyboard.type('Speedgolf123'); //Valid password
       await page.click('#acctPasswordRepeat'); //Focus on 'Repeat Password' field
       await expect(page.locator('#acctPasswordRepeat')).toBeFocused();
-      await page.keyboard.type('Speedgolf1234'); //Password does not match. 
+      await page.keyboard.type('Speedgolf1234'); //Password does not match.
       await page.click('#submitCreateAccountBtn'); //Focus on 'Repeat Password' field
       await expect(page.locator('#acctEmailError')).toBeVisible(); //Email error visible
       await expect(page.locator('#acctPasswordError')).not.toBeVisible(); //No password error
       await expect(page.locator('#acctPasswordRepeatError')).toBeVisible(); //No password error
-      await expect(page.locator('#acctDisplayNameError')).toBeVisible(); //No password error
-      await expect(page.locator('#acctSecurityQuestionError')).toBeVisible(); //No password error
-      await expect(page.locator('#acctSecurityAnswerError')).toBeVisible(); //No password error
-      await expect(page.locator('#acctEmailError')).toBeFocused(); //top-most error should have focus 
+      await expect(page.locator('#acctEmailError')).toBeFocused(); //top-most error should have focus
       await page.click('#acctEmailError'); //Activate "Enter a valid email address"
       await expect(page.locator('#acctEmail')).toBeFocused(); //Email field should be focused
     });
