d29fe2b708866fee70e1a556106c5ff89fc8f1e5
MedusCode <87340589+MedusCode@users.noreply.github.com>
2025-06-06 17:42:51 +0400

    Fix auto Route53 record creating

--- backend-infrastructure/index.ts
+++ backend-infrastructure/index.ts
@@ -3,7 +3,7 @@ import "./cluster";
 import "./alb-controller";
 import "./namespace";
 import "./api";
-import "./route53";
+import "./dns";
 import { albHostname } from "./ingress";
 
 export { albHostname, apiDeploymentUrl };
